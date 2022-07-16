2b059d61bcb0b07039d39eda08c353bd2e88ad97
EscapeFromJava <reske.artur@gmail.com>
2022-07-17 01:39:44 +0300

    v 2.2 merge

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Git: 1.1");
         System.out.println("Git: 1.1.1");
         System.out.println("Git: 2");
+        System.out.println("merge");
     }
 
     public static void test(){
