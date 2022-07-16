8fafe0de907c8ba2e9cd5b37acfea106d9b8a3fb
EscapeFromJava <reske.artur@gmail.com>
2022-07-17 01:39:12 +0300

    v 2.1 main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Git: 1.1");
     }
+
+    public static void test(){
+        System.out.println("test");
+    }
 }
